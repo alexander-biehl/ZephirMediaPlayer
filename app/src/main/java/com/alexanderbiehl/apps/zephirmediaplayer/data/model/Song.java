c071e821009d1b7d0c78af5401e3c3961b970928
Alex Biehl <biehl.al@gmail.com>
2025-02-08 18:30:32 -0500

    added internal version of Song class as an interface with entities for the rest of the app. have some fields like artist/album that may not make sense to keep on a pure song object

--- app/src/main/java/com/alexanderbiehl/apps/zephirmediaplayer/data/model/Song.java
+++ app/src/main/java/com/alexanderbiehl/apps/zephirmediaplayer/data/model/Song.java
@@ -1,4 +1,54 @@
 package com.alexanderbiehl.apps.zephirmediaplayer.data.model;
 
+import android.net.Uri;
+
+import com.alexanderbiehl.apps.zephirmediaplayer.data.entity.SongEntity;
+
 public class Song {
+
+    private Long id;
+    private String mediaId;
+    private String title;
+    private String artist;
+    private String album;
+    private Integer trackNumber;
+    private String sourceUri;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMediaId() {
+        return mediaId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public Integer getTrackNumber() {
+        return trackNumber;
+    }
+
+    public String getSourceUri() {
+        return sourceUri;
+    }
+
+    public static Song from(SongEntity entity) {
+        Song s = new Song();
+        s.id = entity.id;
+        s.mediaId = entity.mediaId;
+        s.title = entity.title;
+        s.sourceUri = entity.sourceUri;
+        s.trackNumber = Integer.valueOf(entity.trackNumber);
+        return s;
+    }
 }
