f640651bb87098d7a1c2127b583dc0424e701afa
Alex Biehl <biehl.al@gmail.com>
2025-02-15 15:52:59 -0500

    Added MediaDataSource implementation for the Database

--- app/src/main/java/com/alexanderbiehl/apps/zephirmediaplayer/datasources/impl/MediaDbDataSource.java
+++ app/src/main/java/com/alexanderbiehl/apps/zephirmediaplayer/datasources/impl/MediaDbDataSource.java
@@ -1,25 +1,63 @@
 package com.alexanderbiehl.apps.zephirmediaplayer.datasources.impl;
 
 import androidx.media3.common.MediaItem;
+import androidx.media3.common.MediaMetadata;
 
 import com.alexanderbiehl.apps.zephirmediaplayer.common.RepositoryCallback;
+import com.alexanderbiehl.apps.zephirmediaplayer.common.Result;
+import com.alexanderbiehl.apps.zephirmediaplayer.data.database.AppDatabase;
+import com.alexanderbiehl.apps.zephirmediaplayer.data.entity.SongEntity;
 import com.alexanderbiehl.apps.zephirmediaplayer.datasources.MediaDataSource;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Executor;
 
 public class MediaDbDataSource implements MediaDataSource {
 
+    private final AppDatabase db;
+    private final Executor executor;
 
-    // TODO this should query the db on a background thread
+    public MediaDbDataSource(
+            AppDatabase db,
+            Executor executor
+    ) {
+        this.db = db;
+        this.executor = executor;
+    }
 
     @Override
     public void getMedia(RepositoryCallback<List<MediaItem>> callback) {
-
+        executor.execute(() -> {
+            List<MediaItem> items = loadMediaFromDb();
+            Result<List<MediaItem>> result = new Result.Success<>(items);
+            callback.onComplete(result);
+        });
     }
 
     @Override
     public List<MediaItem> getMediaSynchronous() {
-        return Collections.emptyList();
+        return loadMediaFromDb();
+    }
+
+    private List<MediaItem> loadMediaFromDb() {
+        SongEntity[] songEntities = db.songDao().getAllSongs();
+        List<MediaItem> items = new ArrayList<>();
+        for (SongEntity e : songEntities) {
+            items.add(new MediaItem.Builder()
+                    .setMediaId(e.mediaId)
+                    .setUri(e.sourceUri)
+                    .setMediaMetadata(
+                            new MediaMetadata.Builder()
+                                    .setTitle(e.title)
+                                    .setArtist(db.artistDao().getById(e.songArtistId).title)
+                                    .setAlbumTitle(db.albumDao().getById(e.songAlbumId).title)
+                                    .setTrackNumber(Integer.valueOf(e.trackNumber))
+                                    .build()
+                    )
+                    .build());
+        }
+        return items;
     }
 }
