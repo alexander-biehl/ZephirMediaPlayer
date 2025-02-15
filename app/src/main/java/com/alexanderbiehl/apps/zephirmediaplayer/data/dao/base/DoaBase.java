f1ba330a733d47c3e1721c02de583852ea134df3
Alex Biehl <biehl.al@gmail.com>
2025-02-15 13:49:51 -0500

    updated insert strategy

--- app/src/main/java/com/alexanderbiehl/apps/zephirmediaplayer/data/dao/base/DoaBase.java
+++ app/src/main/java/com/alexanderbiehl/apps/zephirmediaplayer/data/dao/base/DoaBase.java
@@ -3,6 +3,7 @@ package com.alexanderbiehl.apps.zephirmediaplayer.data.dao.base;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Update;
 
 import com.alexanderbiehl.apps.zephirmediaplayer.data.entity.base.EntityBase;
@@ -10,7 +11,7 @@ import com.alexanderbiehl.apps.zephirmediaplayer.data.entity.base.EntityBase;
 @Dao
 public interface DoaBase<T extends EntityBase> {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     long insert(T entity);
 
 //    @Insert
