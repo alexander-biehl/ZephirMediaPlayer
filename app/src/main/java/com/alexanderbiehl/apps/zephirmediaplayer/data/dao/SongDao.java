65e2b23e368f1f3483b760d16cb3cf385eedb57b
Alex Biehl <biehl.al@gmail.com>
2025-02-15 14:19:15 -0500

    added convenience insertAll method

--- app/src/main/java/com/alexanderbiehl/apps/zephirmediaplayer/data/dao/SongDao.java
+++ app/src/main/java/com/alexanderbiehl/apps/zephirmediaplayer/data/dao/SongDao.java
@@ -7,6 +7,8 @@ import androidx.room.Query;
 import com.alexanderbiehl.apps.zephirmediaplayer.data.dao.base.DoaBase;
 import com.alexanderbiehl.apps.zephirmediaplayer.data.entity.SongEntity;
 
+import java.util.List;
+
 @Dao
 public interface SongDao extends DoaBase<SongEntity> {
 
@@ -21,4 +23,7 @@ public interface SongDao extends DoaBase<SongEntity> {
 
     @Insert
     long[] insertAll(SongEntity... songEntities);
+
+    @Insert
+    List<Long> insertAll(List<SongEntity> entities);
 }
