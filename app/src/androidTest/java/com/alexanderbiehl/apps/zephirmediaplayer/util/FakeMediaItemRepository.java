2726341a6dbf187aa7b86e08cb55d41384d5b82b
Alex Biehl <biehl.al@gmail.com>
2025-05-27 17:48:31 -0400

    added FakeMediaItemRepository stub to see if we can get better instrumentation tests

---
+++ app/src/androidTest/java/com/alexanderbiehl/apps/zephirmediaplayer/util/FakeMediaItemRepository.java
@@ -0,0 +1,5 @@
+package com.alexanderbiehl.apps.zephirmediaplayer.util;
+
+public class FakeMediaItemRepository {
+    // TODO extract inteface from MediaItemRepository so we can use this in tests
+}
